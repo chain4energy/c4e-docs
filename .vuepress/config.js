e98ca9a3ba996da3b7fd45f9a21c3094b615e75e
Panvasi <73217837+Panvasi@users.noreply.github.com>
2023-08-05 14:33:14 +0300

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   theme: 'cosmos',
-  title: 'C4E Documentation',
+  title: 'Βιβλιογραφία C4E',
   locales: {
     '/': {
-      lang: 'en-US'
+      lang: 'el-GR'
     },
   },
   base: process.env.VUEPRESS_BASE || '/',
@@ -29,35 +29,35 @@ module.exports = {
       auto: false,
       nav: [
           {
-            title: 'User guide',
+            title: 'Οδηγός Χρήστη',
             children: [
               {
-                title: 'Introduction',
+                title: 'Εισαγωγή',
                 directory: true,
                 path: '/usersGuide/introduction',
               },
               {
-                title: 'Wallet Basics',
+                title: 'Βασικά στοιχεία για το πορτοφόλι',
                 directory: false,
                 path: '/usersGuide/walletBasics',
               },
               {
-                title: 'Registration',
+                title: 'Εγγραφή',
                 directory: true,
                 path: '/usersGuide/registration',
               },
               {
-                title: 'Authorisation',
+                title: 'Εξουσιοδότηση',
                 directory: false,
                 path: '/usersGuide/auth',
               },
               {
-                title: 'Validators',
+                title: 'Επικυρωτές',
                 directory: true,
                 path: '/usersGuide/validators',
               },
               {
-                title: 'Governance',
+                title: 'Διακυβέρνηση',
                 directory: false,
                 path: '/usersGuide/governance',
               },
@@ -65,48 +65,48 @@ module.exports = {
             ]
           },
         {
-          title: 'Validators Guide',
+          title: 'Οδηγός επικυρωτών',
           children: [
             {
-              title: 'strategic reserve delegation program',
+              title: 'Σρατηγικό πρόγραμμα αντιπροσώπευσης εφεδρειών',
               directory: true,
               path: '/validatorsGuide/strategReserveDelegationProgram',
             },
             {
-              title: 'Start testnet',
+              title: 'Ξεκινήστε το testnet',
               directory: true,
               path: '/validatorsGuide/Starttestnet',
             },
             {
-              title: 'Genesis Fields',
+              title: 'Πεδία genesis',
               directory: true,
               path: '/validatorsGuide/genesisFields',
             },
             {
-              title: 'Start mainnet',
+              title: 'Εκκίνηση mainnet',
               directory: true,
               path: '/validatorsGuide/mainnet',
             },
             {
-              title: 'Modules',
+              title: 'Ενότητες',
               directory: true,
               path: '/validatorsGuide/modules',
             },
           ]
         },
         {
-          title: 'Our products',
+          title: 'Τα προϊόντα μας',
           children: [
             {
-              title: 'Billing system',
+              title: 'Σύστημα χρέωσης',
               path: 'https://platformadlaenergii.pl'
             },
             {
-              title: 'Wallet',
+              title: 'Πορτοφόλι',
               path: 'https://wallet.c4e.io'
             },
             {
-              title: 'explorer',
+              title: 'εξερευνητής',
               path: 'https://explorer.c4e.io'
             }
           ]
@@ -114,30 +114,30 @@ module.exports = {
       ]
     },
     gutter: {
-      title: 'Help & Support',
+      title: 'Βοήθεια και υποστήριξη',
       chat: {
-        title: 'Developer Chat',
-        text: 'Chat with C4E teams on telegram.',
+        title: 'Συνομιλία προγραμματιστών',
+        text: 'Συνομιλήστε με ομάδες C4E στο telegram',
         url: 'https://t.me/+60rmPWzlo3c2YWY8',
         bg: 'linear-gradient(103.75deg, #1B1E36 0%, #22253F 100%)',
       },
       telegram: {
-        title: 'Telegram Chat',
-        text: 'Chat with C4E teams on telegram.',
+        title: 'Συνομιλία προγραμματιστών',
+        text: 'Συνομιλήστε με ομάδες C4E στο telegram',
         url: 'https://t.me/Chain4Energy_Official',
         bg: 'linear-gradient(103.75deg, #1B1E36 0%, #22253F 100%)',
         logo: '/logo.svg',
       },
       forum: {
-        title: 'Cosmos Developer Forum',
-        text: 'Cosmos Developer Forum to learn more.',
+        title: 'Φόρουμ προγραμματιστών Cosmos',
+        text: 'Φόρουμ προγραμματιστών Cosmos για να μάθετε περισσότερα.',
         url: 'https://forum.cosmos.network/',
         bg: 'linear-gradient(221.79deg, #3D6B99 -1.08%, #336699 95.88%)',
         logo: 'ethereum-white'
       },
       github: {
-        title: 'Found an Issue?',
-        text: 'Help us improve this page by suggesting edits on GitHub.',
+        title: 'Βρήκατε ένα πρόβλημα;',
+        text: 'Βοηθήστε μας να βελτιώσουμε αυτήν τη σελίδα προτείνοντας αλλαγές στο GitHub.',
         bg: '#F8F9FC'
       }
     },
@@ -159,36 +159,36 @@ module.exports = {
           url: 'https://www.linkedin.com/company/chain4energy/'
         },
       ],
-      smallprint: 'This website is maintained by C4E team',
+      smallprint: 'Αυτή η ιστοσελίδα διατηρείται από την ομάδα C4E',
       links: [{
-          title: 'Documentation',
+          title: 'Βιβλιογραφία ',
           children: [{
               title: 'Cosmos SDK Docs',
               url: 'https://docs.cosmos.network'
             },
             {
-              title: 'Tendermint Core Docs',
+              title: 'Βασικά Έγγραφα Tendermint',
               url: 'https://docs.tendermint.com'
             }
           ]
         },
         {
-          title: 'Community',
+          title: 'Κοινότητα',
           children: [{
-              title: 'Cosmos Community',
+              title: 'Κοινότητα Cosmos',
               url: 'https://discord.gg/W8trcGV'
             }
           ]
         },
         {
-          title: 'Contributing',
+          title: 'Συμβάλλετε',
           children: [{
-              title: 'Contributing to the docs',
+              title: 'Συμβάλλετε στην βιβλιογραφία',
               url: 'https://github.com/chain4energy/c4e-docs'
             },
 
             {
-              title: 'Source code on GitHub',
+              title: 'Πηγαίος κώδικας στο GitHub',
               url: 'https://github.com/chain4energy/c4e-chain'
             }
           ]
